8f827e886db7e0f06d2e4ee30c8c4659f4875511
nishigupta9 <guptanishiusa@gmail.com>
2025-08-29 12:36:06 -0400

    Update runExample.py

--- Example/runExample.py
+++ Example/runExample.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 import os
-os.system('./run_scTIGER.py -goi JUNB+STAT5B+ATF5+HES1+MXD1 -exp ../Sample_Data/K562/K562.csv -p 100 -top 50 -zero 0.00 -o SampleResult_K562')
+os.system('./run_scTIGER.py -goi JUNB+STAT5B+ATF5+HES1+MXD1 -exp ./Sample_Data/K562/K562.csv -p 100 -top 50 -zero 0.00 -o SampleResult_K562')
